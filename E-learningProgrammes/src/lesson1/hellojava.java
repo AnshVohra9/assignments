c1da1be3844f15b19d095f975fee71cc132c2b6a
ansh vohra <anshvohra20k@gmail.com>
2022-07-25 12:05:47 +0530

    e learning

---
+++ E-learningProgrammes/src/lesson1/hellojava.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to java world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
