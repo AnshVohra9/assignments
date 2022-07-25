034a642c494740e40f2dbf61fe0083ceb194b798
ansh vohra <anshvohra20k@gmail.com>
2022-07-25 16:05:52 +0530

    Strings2

---
+++ assignments/src/assignments/strings.java
@@ -0,0 +1,48 @@
+package assignments;
+
+public class strings {
+
+	public static void main(String[] args) {
+		String t="Delhi";
+		String o="Mumbai";
+		String k="delhi";
+		String y=new String("Mumbai");
+		String l=new String("delhi");
+		String p=new String("Hello");
+		if (o.equals(l) && o==l)
+		{
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+		if (y.equals(p) && y==p) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+		if (t.equals(o) && t==o) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+		if (k.equals(y) && k==y) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+		}
+		if (p.equals(y) && p==y) {
+			System.out.println("True");
+		}
+		else {
+			System.out.println("False");
+	
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
